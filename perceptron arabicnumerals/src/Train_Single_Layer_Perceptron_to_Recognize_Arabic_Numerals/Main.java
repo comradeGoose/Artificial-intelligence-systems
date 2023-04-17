d26d6cc16a150f01da095c4f2281e898873bbb82
Grebenyuk Alexey <78959401+comradeGoose@users.noreply.github.com>
2023-04-17 20:04:04 +0900

    add

---
+++ perceptron arabicnumerals/src/Train_Single_Layer_Perceptron_to_Recognize_Arabic_Numerals/Main.java
@@ -0,0 +1,8 @@
+package Train_Single_Layer_Perceptron_to_Recognize_Arabic_Numerals;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ perceptron_arabic_numerals/src/perceptron_arabic_numerals/Main.java
@@ -0,0 +1,9 @@
+package perceptron_arabic_numerals;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArabicNumerals arabic = new ArabicNumerals();
+        arabic.test();
+    }
+}
